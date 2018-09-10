8030e6535c5e6d1cb4829d2c30d0a13bd00e8351
gokilaguna1998 <36723035+gokilaguna1998@users.noreply.github.com>
2018-09-10 14:33:44 +0530

    Create location.java

---
+++ location.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class GFG
+{
+public Static void main(String [] args)
+{
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int c=2*n;
+int count=0;
+for(int k=0,j=1;k<c && j<c;i+=2,j+=2)
+{
+System.out.println(k+" "+j);
+count++;
+}
+System.out.println(count);
+}
